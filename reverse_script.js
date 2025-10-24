36b27ee1c29cd45262c3129f7dfbdbaae338175f
AndyTheDragon <andre.jansam@gmail.com>
2025-10-24 15:48:55 +0200

    "Game loop" completed, it's now possible to play the game.

--- reverse_script.js
+++ reverse_script.js
@@ -1,10 +1,13 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", main);
+var currentGuess, minGuess, maxGuess;
 
 function main() {
     console.log("Hello, World!");
+
     document.getElementById("guess-form").addEventListener("change", (event) => { console.log("Feedback changed. " + event.target.id + " was selected"); });
+    document.getElementById("start-game").addEventListener("click", initGame);
 }
 
 function performGuess(prevGuess, tooLow) {
@@ -21,15 +24,15 @@ function addGuessToList(guess) {
     li.insertAdjacentHTML('beforeend', `I'm guessing ${guess} - <div class="d-inline" id="guess-feedback">is that 
                         <div class="form-check form-check-inline">
                             <input class="btn-check" type="radio" id="too-low" name="feedback40" value="low">
-                            <label class="btn btn-sml btn-primary" for="too-low">too low</label>
+                            <label class="btn btn-sm btn-primary" for="too-low">too low</label>
                         </div>
                         <div class="form-check form-check-inline">
                             <input class="btn-check" type="radio" id="too-high" name="feedback40" value="high">
-                            <label class="btn btn-sml btn-primary" for="too-high">too high</label>
+                            <label class="btn btn-sm btn-primary" for="too-high">too high</label>
                         </div>
                         <div class="form-check form-check-inline">
                             <input class="btn-check" type="radio" id="correct" name="feedback40" value="correct">
-                            <label class="btn btn-sml btn-primary" for="correct">correct</label>
+                            <label class="btn btn-sm btn-primary" for="correct">correct</label>
                         </div>
                         ?</div>`);
     ul.appendChild(li);
@@ -37,20 +40,35 @@ function addGuessToList(guess) {
 
 function handleClick(event) {
     let feedback = "";
+    if (event.target.id === "correct") {
+        feedback = "User indicated the guess was correct.";
+        document.getElementById("guess-feedback").replaceWith(feedback);
+        document.getElementById("start-game").textContent = "Start New Game";
+        document.getElementById("start-game").disabled = false;
+        return;
+    } 
     if (event.target.id === "too-low") {
         feedback = "User indicated the guess was too low.";
+        currentGuess = performGuess(currentGuess, true);
     } else if (event.target.id === "too-high") {
         feedback = "User indicated the guess was too high.";
-    } else if (event.target.id === "correct") {
-        feedback = "User indicated the guess was correct.";
+        currentGuess = performGuess(currentGuess, false);   
     }
     document.getElementById("guess-feedback").replaceWith(feedback);
+    addGuessToList(currentGuess);
+    document.getElementById("guess-form").addEventListener("change", handleClick);
+
 }
 
-function initGame() {
-    let currentGuess = 50;
-    let minGuess = 1;
-    let maxGuess = 100;
+function initGame(event) {
+    currentGuess = 50;
+    minGuess = 1;
+    maxGuess = 100;
+    event.preventDefault();
+    const ul = document.getElementById("guess-list");
+    ul.innerHTML = "";
+    event.target.disabled = true;
+    event.target.textContent = "Game in progress...";
     addGuessToList(currentGuess);
     document.getElementById("guess-form").addEventListener("change", handleClick);
     
