0948d5f7c11f757c521f5bfc39d5d58a9c15bd43
AndyTheDragon <andre.jansam@gmail.com>
2025-10-24 19:00:31 +0200

    Implement binary search and error handling

--- reverse_script.js
+++ reverse_script.js
@@ -4,33 +4,37 @@ window.addEventListener("DOMContentLoaded", main);
 var currentGuess, minGuess, maxGuess;
 
 function main() {
-    console.log("Hello, World!");
-
     document.getElementById("guess-form").addEventListener("change", (event) => { console.log("Feedback changed. " + event.target.id + " was selected"); });
     document.getElementById("start-game").addEventListener("click", initGame);
 }
 
 function performGuess(prevGuess, tooLow) {
     if (tooLow) {
-        return prevGuess + 1;
+        minGuess = prevGuess + 1;
     } else {
-        return prevGuess - 1;
+        maxGuess = prevGuess - 1;
     }
+    let nextGuess = minGuess + Math.floor((maxGuess - minGuess) / 2);
+    if (nextGuess < minGuess || nextGuess > maxGuess || nextGuess === prevGuess) {
+        nextGuess = 0;
+    }
+    return nextGuess;
 }
 
 function addGuessToList(guess) {
     const ul = document.getElementById("guess-list");
     const li = document.createElement("li");
+    li.classList.add("list-group-item");
     li.insertAdjacentHTML('beforeend', `I'm guessing ${guess} - <div class="d-inline" id="guess-feedback">is that 
-                        <div class="form-check form-check-inline">
+                        <div class="form-check mx-1 px-0 form-check-inline">
                             <input class="btn-check" type="radio" id="too-low" name="feedback40" value="low">
                             <label class="btn btn-sm btn-primary" for="too-low">too low</label>
                         </div>
-                        <div class="form-check form-check-inline">
+                        <div class="form-check mx-1 px-0 form-check-inline">
                             <input class="btn-check" type="radio" id="too-high" name="feedback40" value="high">
                             <label class="btn btn-sm btn-primary" for="too-high">too high</label>
                         </div>
-                        <div class="form-check form-check-inline">
+                        <div class="form-check mx-1 px-0 form-check-inline">
                             <input class="btn-check" type="radio" id="correct" name="feedback40" value="correct">
                             <label class="btn btn-sm btn-primary" for="correct">correct</label>
                         </div>
@@ -54,6 +58,14 @@ function handleClick(event) {
         feedback = "User indicated the guess was too high.";
         currentGuess = performGuess(currentGuess, false);   
     }
+    if (currentGuess === 0 || currentGuess === 100) {
+        feedback = "I think something went wrong. Please start a new game.";
+        document.getElementById("guess-feedback").replaceWith(feedback);
+        document.getElementById("start-game").textContent = "Start New Game";
+        document.getElementById("start-game").disabled = false;
+        return;
+    }
+    
     document.getElementById("guess-feedback").replaceWith(feedback);
     addGuessToList(currentGuess);
     document.getElementById("guess-form").addEventListener("change", handleClick);
